17de0f161696e90fbbab3dbedda0c055bfaec6f8
zhangzl <zhangzl@cadg.cn>
2022-08-19 18:13:11 +0800

    z

--- DanielleSpider/items.py
+++ DanielleSpider/items.py
@@ -3,12 +3,15 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from turtle import title
 import scrapy
 
 
 class DaniellespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    name = scrapy.Field()
+    title = scrapy.Field()
     source = scrapy.Field()
     url = scrapy.Field()
+    bulletinIssueTime = scrapy.Field()
+    sheetName = scrapy.Field()
--- DanielleSpider/pipelines.py
+++ DanielleSpider/pipelines.py
@@ -6,8 +6,27 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import xlsxwriter
 
 class DaniellespiderPipeline:
+    def open_spider(self, spider):
+        self.workbook = xlsxwriter.Workbook('hello.xlsx')
+        self.workbook.add_worksheet("招标信息汇总")
+        self.workbook.add_worksheet("中标信息汇总")
+        self.countMap = {'招标信息汇总': 1, '中标信息汇总': 1}
+
+    def close_spider(self, spider):
+        self.workbook.close()
+
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        sheetName = adapter['sheetName']
+        sheet = self.workbook.get_worksheet_by_name(sheetName)
+        count = self.countMap[sheetName]
+        countStr = str(count)
+        sheet.write('A'+countStr, adapter['source'])
+        sheet.write('B'+countStr, adapter['bulletinIssueTime'])
+        sheet.write('C'+countStr, adapter['title'])
+        sheet.write('D'+countStr, adapter['url'])
+        self.countMap[sheetName] = count + 1
         return item
